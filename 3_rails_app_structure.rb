969bef2db063c1ad796bdcf0d681344a0462c41c
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-07-07 22:23:38 -0400

    Added rails app structure notes.

---
+++ 3_rails_app_structure.rb
@@ -0,0 +1,56 @@
+# app/ = Contains the controllers, models, views, helpers, 
+#        mailers, channels, jobs, and assets for your
+#        application.
+
+# bin/ = Contains the rails script that starts your app and 
+#        can contain other scripts you use to set up, update, 
+#        deploy, or run your application.
+
+# config/ = Contains configuration for your application's 
+#           routes, databases, and more.
+
+# config.ru = Rack configuration for Rack-based servers used 
+#             to start the application.
+
+# db/ = Contains your current database schema, as well as the 
+#       database migrations.
+
+# Gemfile + Gemfile.lock = These files allow you to specify 
+#           what gem dependencies are needed for your Rails 
+#           application. These files are used by the Bundler gem.
+
+# lib/ = Extended modules for your application
+
+# log/ = Application log files
+
+# package.json = This file allows you to spcify what npm 
+#                dependencies are needed for your Rails 
+#                application. This file is used by Yarn.
+
+
+# public/ = Contains static files and compiled assets. When your 
+#           app is running, this directory will be exposed as is.
+
+# Rakefile = This file locates and loads tasks that can be run 
+#            from the command line. The task definitions are defined 
+#            throughout the components of Rails. Rather than changing 
+#            Rakefile, you should add your own tasks by adding files 
+#            to the lib/tasks directory of your application.
+
+# README.md = This is a brief instruction manual for your application.
+#             You should edit this file to tell others what your 
+#             application does, how to set it up, and so on.
+
+# storage/ = Active Storage files for Disk Service
+
+# test/ = Unit tests, fixtures, and other test apparatus.
+
+# tmp/ = Temporary files (like cache and pid files)
+
+# vendor/ = A place for all third-party code. In a typical Rails
+#           application this includes vendored gems.
+
+# .gitignore = This file tells git which files (or patterns) it 
+#              should ignore.
+
+# .ruby-version = This file contains the default Ruby version.
