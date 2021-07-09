808a8a974febeca4f06b14e53366b45bd2dff567
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-07-09 16:15:22 -0400

    Added html css notes

---
+++ 6_html_css.rb
@@ -0,0 +1,9 @@
+# HTML basics: https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics
+
+# CSS basics: https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/CSS_basics
+
+# HTML tables: https://developer.mozilla.org/en-US/docs/Learn/HTML/Tables
+
+# HTML forms: https://developer.mozilla.org/en-US/docs/Learn/Forms/Your_first_form
+
+# Alternate resource to learn HTML and CSS together while building a website: https://learn.shayhowe.com/html-css/
