864d8dfa103d224c6ff4285b004ed48111aed88d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-07-09 16:09:53 -0400

    Added git notes

---
+++ 4_git.rb
@@ -0,0 +1,9 @@
+# Use git to save versions of the app. 
+# this way you can go back to an early version of your app if you mess up
+# You can use github repository to save your commits 
+# you can share your code with others on github
+# git add . = adds all files
+# git status = shows status
+# git commit -m "" = add a commit with a message for the files added
+# git push = pushes commit to github
+# use .gitignore if you don't want to track certain files on github
