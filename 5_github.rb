71f82420493e3183fc982e26fd4cfac2d30b7579
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-07-09 16:14:00 -0400

    Added github notes

---
+++ 5_github.rb
@@ -0,0 +1,6 @@
+# Could also use bitbucket, but using github
+# create an account
+# you can create public and private repositories
+# Follow the directions on how to push data to the newly created repository
+# You can create an SSH key for security
+# user > settings > SSH and GPG keys
