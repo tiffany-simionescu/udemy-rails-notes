b156f9129fc387b490bc1391da937ff668c8109d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-07-10 13:50:28 -0400

    Added about page notes.

---
+++ 7_about_page.rb
@@ -0,0 +1,10 @@
+# config > routes
+# get 'about', to: 'pages#about'
+# adding action about to the controller we already created, pages
+# go to app > controllers > pages_controller.rb
+# add about method/action
+# def about
+# end
+# go to app > views > layouts > pages > New File > about.html.erb
+# write out your html
+# go to localhost:3000/about to view your new about page
